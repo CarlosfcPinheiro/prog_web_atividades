dbf3fbef62501567d2c2b4de4b135530e53d513f
CarlosfcPinheiro <carlosfcpinheiro82@gmail.com>
2024-09-23 21:28:23 -0300

    organizando exercicio e adicionando micraft fetch api

---
+++ ex06-fetchStarWars/scripts/mineserver.js
@@ -0,0 +1,26 @@
+const input_address = document.getElementById('input-address');
+const send_button = document.getElementById('send-button');
+const output = document.getElementById('output');
+
+const fetchData = async (url) => {
+    // try and catch block
+    try{
+        // GET request
+        const promiseFetch = await fetch(url);
+
+        const jsonData = await promiseFetch.json();
+        const formatedText = JSON.stringify(jsonData, null, 2);
+        output.textContent = formatedText;
+        
+    } catch(err){
+        console.log(`Error: ${err}`);
+    }
+}
+
+send_button.addEventListener('click', () => {
+    const address = input_address.value;
+    console.log(address);
+    const url = `https://api.mcsrvstat.us/3/${address}`;
+
+    fetchData(url);
+});
